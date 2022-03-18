1d7c33c509ff68b9d1fc493233967cd792a95299
wataru5354 <setsunan76253@gmail.com>
2022-03-18 12:57:37 +0900

    商品削除機能のコード再々修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path if @item.destroy
+    if  current_user.id == @item.user_id
+      if @item.destroy
+        redirect_to root_path 
+      else
+        render :show
+      end
     end
   end
 
