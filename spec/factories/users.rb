c83d458d6b0f4b76318aa3682fcf1d474a46dd23
wataru5354 <setsunan76253@gmail.com>
2022-03-21 17:59:23 +0900

    rubocopによる修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
 
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password              { "1a#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     last_name             { person.last }
     first_name            { person.first }
