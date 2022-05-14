e7a65382334df6f85a32843a28d805b3cf3880d7
wataru5354 <setsunan76253@gmail.com>
2022-05-14 09:17:39 +0900

    コード修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,12 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :set_item
-
+  before_action :set_card
   def index
     @purchase_address = PurchaseAddress.new
     redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
     card = Card.find_by(user_id: current_user.id)
-
+    set_card
     if card.present?
       customer = Payjp::Customer.retrieve(card.customer_token)
       @card = customer.cards.first
@@ -15,13 +15,11 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    set_card
     card = Card.find_by(user_id: current_user.id)
-    if card.present?
-      customer = Payjp::Customer.retrieve(card.customer_token)
-      @card = customer.cards.first
-      @purchase_address.token = current_user.card.customer_token
-    end
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
+    @purchase_address.token = current_user.card.customer_token
     if @purchase_address.valid?
       pay_item
       @purchase_address.save
@@ -43,20 +41,15 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def set_card
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  end
+
   def pay_item
-    if @card.present?
-      customer_token = current_user.card.customer_token
-      Payjp::Charge.create(
-        amount: @item.price,
-        customer: customer_token,
-        currency: 'jpy'
+    Payjp::Charge.create(
+      amount: @item.price,
+      customer: @purchase_address.token,
+      currency: 'jpy'
       )
-    else
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: purchase_params[:token],
-        currency: 'jpy'
-      )
-    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../price")
-require("../card")
 require("../preview")
 require("../card_save")
 // Uncomment to copy all static images under ../images to the output folder and reference
